c7336dc00d5110f9ae68a7bffea7d91c8e346bd9
takeuchi-gh <ytakejizaiten@gmail.com>
2020-01-13 15:26:44 +0900

    users_function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
-
-  def hello
-    render html: "hello,world"
-  end
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -16,6 +14,23 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :nickname,
+      :profile_img,
+      :profile_text,
+      :family_name,
+      :first_name,
+      :family_name_kana,
+      :first_name_kana,
+      :birth_year,
+      :birth_month,
+      :birth_day,
+      :phone_num])
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      render :edit
+      # flash
+    else
+      render :edit
+      # flash
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :profile_text)
+  end
 end

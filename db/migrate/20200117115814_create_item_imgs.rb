7094731f4a1848e7de49d5da624075822e0adb7f
shutoskate <wizwizwayne@gmail.com>
2020-01-17 21:16:02 +0900

    fix

--- db/migrate/20200117115814_create_item_imgs.rb
+++ db/migrate/20200117115814_create_item_imgs.rb
@@ -1,7 +1,7 @@
 class CreateItemImgs < ActiveRecord::Migration[5.0]
   def change
     create_table :item_imgs do |t|
-      t.string :img        null: false,
+      t.string :img,       null: false
       t.integer :item_id,  null: false, foreign_key: true
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200117034140) do
+ActiveRecord::Schema.define(version: 20200117115814) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20200117034140) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_categories_on_name", using: :btree
+  end
+
+  create_table "item_imgs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "img",        null: false
+    t.integer  "item_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|

6bea16675e75e57be7a649da606428d1f5db2730
takeuchi-gh <ytakejizaiten@gmail.com>
2020-01-17 22:02:11 +0900

    create_seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+  # Character.create(name: 'Luke', movie: movies.first)
+
+# brands = []
+# 50.times do |i|
+#   brands << Brand.new(name: "Brand-#{i+1}")
+# end
+
+100.times do |i|
+  Brand.create!(
+    name: "Brand #{i+1}",
+  )
+end
+require "csv"
+
+CSV.foreach('db/category.csv') do |row|
+  Category.create(:name => row[0], :parent_id => row[1])
+end

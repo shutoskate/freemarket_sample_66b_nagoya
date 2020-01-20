4e898d0a63d3fe6593110da55c35db6881640da1
takeuchi-gh <ytakejizaiten@gmail.com>
2020-01-21 02:12:08 +0900

    add_brand_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,17 @@
 #   brands << Brand.new(name: "Brand-#{i+1}")
 # end
 
-100.times do |i|
-  Brand.create!(
-    name: "Brand #{i+1}",
-  )
-end
+# 100.times do |i|
+#   Brand.create!(
+#     name: "Brand #{i+1}",
+#   )
+# end
+
 require "csv"
+
+CSV.foreach('db/brand.csv') do |row|
+  Brand.create(:name => row[0])
+end
 
 CSV.foreach('db/category.csv') do |row|
   Category.create(:name => row[0], :parent_id => row[1])

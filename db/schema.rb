399eb136e7abf4020872a20cff5366c37175daac
takeuchi-gh <ytakejizaiten@gmail.com>
2020-01-21 15:14:32 +0900

    edit_migration_file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200120153957) do
+ActiveRecord::Schema.define(version: 20200121021718) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -86,8 +86,8 @@ ActiveRecord::Schema.define(version: 20200120153957) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
-    t.string   "phone_num"
     t.date     "birthday",                                          null: false
+    t.string   "phone_num",                                         null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["nickname"], name: "index_users_on_nickname", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree

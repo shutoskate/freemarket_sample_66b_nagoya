62e1dc718493cc54fced44aadb0bb476c2aa4921
githubuserryu <ootaryunosuke@gmail.com>
2020-01-21 19:18:14 +0900

    商品一覧-create

--- db/schema.rb
+++ db/schema.rb
@@ -84,12 +84,12 @@ ActiveRecord::Schema.define(version: 20200119023459) do
     t.integer  "birth_year",                                        null: false
     t.integer  "birth_month",                                       null: false
     t.integer  "birth_day",                                         null: false
+    t.string   "phone_num",                                         null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
-    t.string   "phone_num"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["nickname"], name: "index_users_on_nickname", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree

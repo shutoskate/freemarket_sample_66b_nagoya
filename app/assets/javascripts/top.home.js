cc02b2e4ba64879d567482fe6a8862cf267b278f
githubuserryu <ootaryunosuke@gmail.com>
2020-01-22 22:03:39 +0900

    スクロール
    パンくず終了
    スクロール、カテゴリー終了
    ブランド未完成

--- app/assets/javascripts/top.home.js
+++ app/assets/javascripts/top.home.js
@@ -1,15 +1,36 @@
 $(function(){
 
   // カテゴリー等選択時のスクロール---------------------
-  $(".select-list").on("click",function(){
-    var num = $(".select-list").index(this);
-    var scroll = $(".top__category-item__category-list__top--name"+[num]).offset().top;
-    $("html,body").animate({
-      scrollTop:scroll
-    })
-  });
 
-
-
+   // #で始まるアンカーをクリックした場合に処理
+   $('a[href^="#"]').click(function() {
+     console.log("aa")
+      // スクロールの速度
+      var speed = 400; // ミリ秒
+      // アンカーの値取得
+      var href= $(this).attr("href");
+      // 移動先を取得
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      // 移動先を数値で取得
+      var position = target.offset().top;
+      // スムーススクロール
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+   });
+   
+   $('a[href^="#"]').not('a[href^="#"]').click(function() {
+     console.log("bb")
+      // スクロールの速度
+      var speed = 400; // ミリ秒
+      // アンカーの値取得
+      var href= $(this).attr("href");
+      // 移動先を取得
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      // 移動先を数値で取得
+      var position = target.offset().top;
+      // スムーススクロール
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+   });
 
 });

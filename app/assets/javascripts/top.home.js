81150a44c7c07a2cf95b7b8ab4abad4957615c79
githubuserryu <ootaryunosuke@gmail.com>
2020-01-23 16:19:43 +0900

    Update top.home.js

--- app/assets/javascripts/top.home.js
+++ app/assets/javascripts/top.home.js
@@ -1,18 +1,11 @@
 $(function(){
-
   // カテゴリー等選択時のスクロール---------------------
-  //  #で始まるアンカーをクリックした場合に処理
    $('a[href^="#"]').on('click', function(){
      console.log("aa")
-     // スクロールの速度
-     var speed = 400; // ミリ秒
-     // アンカーの値取得
+     var speed = 400; 
      var href= $(this).attr("href");
-     // 移動先を取得
      var target = $(href == "#" || href == "" ? 'html' : href);
-     // 移動先を数値で取得
      var position = target.offset().top;
-     // スムーススクロール
      $('body,html').animate({scrollTop:position}, speed, 'swing');
      return false;
     });

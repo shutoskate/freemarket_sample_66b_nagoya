6ea824c193948be62845056ac929ddd99b7b5fac
shutoskate <wizwizwayne@gmail.com>
2020-01-24 15:23:07 +0900

    show

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -37,7 +37,6 @@ class CardsController < ApplicationController
   end
 
   def show #Cardのデータpayjpに送り情報を取り出します
-    @card = Card.find_by(user_id: current_user.id)
     if @card.blank?
       redirect_to action: "new" 
     else

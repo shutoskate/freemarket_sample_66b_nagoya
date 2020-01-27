715e77ae291be250826347096a0d7ffd2daae22b
takeuchi-gh <ytakejizaiten@gmail.com>
2020-01-27 15:42:47 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,81 @@
-table_names = %w(items)
-
-table_names.each do |table_name|
-  environment = (Rails.env == "test") ? "development" : Rails.env
-
-  path = Rails.root.join("db/seeds", environment, table_name + ".rb")
-  if File.exist?(path)
-    puts "#{table_name}..."
-    require path
-  end
+10.times do |i|
+  Item.create!(
+    name: "レディースItem #{i+1}",
+    size: 2,
+    price: 12345,
+    seller_id: 1,
+    brand_id: 333,
+    category_id: 170,
+    status: 1,
+    charge: 1,
+    trade_step: 1,
+    delivery: 0,
+    prefecture: 3,
+    term: 1,
+    item_text: "text"
+  )
 end
+10.times do |i|
+  Item.create!(
+    name: "メンズItem #{i+1}",
+    size: 2,
+    price: 12345,
+    seller_id: 1,
+    brand_id: 333,
+    category_id: 350,
+    status: 1,
+    charge: 1,
+    trade_step: 1,
+    delivery: 0,
+    prefecture: 3,
+    term: 1,
+    item_text: "text"
+  )
+end
+10.times do |i|
+  Item.create!(
+    name: "ホビーItem #{i+1}",
+    size: 2,
+    price: 12345,
+    seller_id: 1,
+    brand_id: 333,
+    category_id: 770,
+    status: 1,
+    charge: 1,
+    trade_step: 1,
+    delivery: 0,
+    prefecture: 3,
+    term: 1,
+    item_text: "text"
+  )
+end
+10.times do |i|
+  Item.create!(
+    name: "家電Item #{i+1}",
+    size: 2,
+    price: 12345,
+    seller_id: 1,
+    brand_id: 333,
+    category_id: 960,
+    status: 1,
+    charge: 1,
+    trade_step: 1,
+    delivery: 0,
+    prefecture: 3,
+    term: 1,
+    item_text: "text"
+  )
+end
+
+
+# table_names = %w(items)
+
+# table_names.each do |table_name|
+#   environment = (Rails.env == "test") ? "development" : Rails.env
+
+#   path = Rails.root.join("db/seeds", environment, table_name + ".rb")
+#   if File.exist?(path)
+#     puts "#{table_name}..."
+#     require path
+#   end
+# end

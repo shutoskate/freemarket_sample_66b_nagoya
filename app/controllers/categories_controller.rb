c259e9aa3a0436a9088576b2467829d294c6e03c
takeuchi-gh <ytakejizaiten@gmail.com>
2020-01-28 17:15:58 +0900

    category_search_function

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,22 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+  def show
+    @category = Category.find(params[:id])
+    if @category.parent_id == nil
+      @categories = [
+        @category.children,
+        @category.children.map { |category| category.children }
+      ].flatten.compact
+    elsif @category.children == nil
+      @categories = @category
+    else 
+      @categories = [
+        @category,
+        @category.children
+      ].flatten.compact
+    end
+    @items = @categories.map{|category| Item.where(category_id: category.id)}.flatten
+  end
+end
--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -22,6 +22,18 @@ end
 
 # カテゴリーとブランドの詳細ページを作成次第パンクズを以下に作成
 
+# カテゴリー一覧
+crumb :categories do
+  link "カテゴリー一覧", categories_path
+end
+
+# カテゴリー検索
+crumb :category do
+  @category = Category.find(params[:id])
+  link "#{@category.name}", category_path(@category)
+  parent :categories
+end
+
   # crumb :パンくず名 do
   #   link "パンくずに表示される文字", パス
   #   parent :必要に応じて、親ページのパンくず名を記述

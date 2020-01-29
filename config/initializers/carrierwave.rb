60ab0a20f93a62e85673602dee7c32deea6cc93f
githubuserryu <ootaryunosuke@gmail.com>
2020-01-29 17:51:51 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,7 +15,7 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1' # 東京はap-northeast-1
     }
     config.fog_directory  = 'freemarket66b' #S3のバケット名
-    config.asset_host = 'https://s3-your_region.amazonaws.com'
+    config.asset_host = 'https://freemarket66b.s3.amazonaws.com'
   else
     config.storage :file # 開発環境:public/uploades下に保存
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
